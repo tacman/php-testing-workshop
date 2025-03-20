4f6f35f56b06c07e494e26f9ef5af2bfb2254eda
Hugo Hamon <hello@kodero.fr>
2025-03-20 11:27:11 +0100

    Add Agenda stubbing example

--- tests/Unit/Entity/AgendaSlotTest.php
+++ tests/Unit/Entity/AgendaSlotTest.php
@@ -12,16 +12,25 @@ use App\Entity\MedicalSpecialty;
 use DateTimeImmutable;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Uid\Uuid;
 
 final class AgendaSlotTest extends TestCase
 {
+    private Agenda $agenda;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->agenda = $this->createDummyAgenda();
+    }
+
     public function testCreateOpenAgendaSlot(): void
     {
-        // Arrange
-        $agenda = new Agenda(
-            new HealthSpecialist('John', 'Doe', MedicalSpecialty::CARDIOLOGIST),
-        );
+        $agenda = $this->createStub(Agenda::class);
+        $agenda->method('getId')->willReturn(Uuid::fromString('e39ad697-c554-421f-b381-86945500c123'));
 
+        // Arrange
         $sut = AgendaSlot::createOpen($agenda, '2025-03-20 09:00', '2025-03-20 09:30');
 
         // Act & Assert
@@ -37,16 +46,18 @@ final class AgendaSlotTest extends TestCase
 
     public function testCreateAgendaSlotWithSwitchedOpeningAndClosingTimes(): void
     {
-        // Arrange
-        $agenda = new Agenda(
-            new HealthSpecialist('John', 'Doe', MedicalSpecialty::CARDIOLOGIST),
-        );
-
         // Assert
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Opening time must be before closing time');
 
         // Act
-        AgendaSlot::createOpen($agenda, '2025-03-20 09:30', '2025-03-20 09:00');
+        AgendaSlot::createOpen($this->agenda, '2025-03-20 09:30', '2025-03-20 09:00');
+    }
+
+    private function createDummyAgenda(): Agenda
+    {
+        return new Agenda(
+            new HealthSpecialist('John', 'Doe', MedicalSpecialty::CARDIOLOGIST),
+        );
     }
 }
