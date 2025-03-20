9a72ffe51bdfb1d0fe66a05740eeb5d1e66aeac6
Hugo Hamon <hello@kodero.fr>
2025-03-20 15:39:17 +0100

    Add more test cases for appointment cancellation API endpoints

--- tests/Functional/Controller/Api/PostAppointmentsCancellationControllerTest.php
+++ tests/Functional/Controller/Api/PostAppointmentsCancellationControllerTest.php
@@ -67,6 +67,65 @@ final class PostAppointmentsCancellationControllerTest extends WebTestCase
         ]);
     }
 
+    public function testAppointmentCancellationValidationFails(): void
+    {
+        $appointment = MedicalAppointmentFactory::new()
+            ->scheduled('E4N6ST')
+            ->tomorrowAt('10:00')
+            ->forPatient('John', 'Smith')
+            ->create();
+
+        self::ensureKernelShutdown();
+
+        $this->browser()
+            ->withProfiling()
+            ->post(
+                url: \sprintf('/api/appointments/%s/cancellation', $appointment->getId()),
+                options: HttpOptions::json([
+                    'referenceNumber' => 'FOOBAR',
+                    'lastName' => 'DOUGLAS',
+                    'reason' => 'I booked another one earlier.',
+                ]),
+            )
+            ->assertStatus(422)
+            ->assertJson()
+            ->assertJsonMatches('violations[0].[propertyPath, title]', ['referenceNumber', 'Invalid appointment reference number.'])
+            ->assertJsonMatches('violations[1].[propertyPath, title]', ['lastName', 'Invalid appointment patient last name.'])
+            ->use(function (MailerComponent $component): void {
+                $component->assertNoEmailSent();
+            })
+        ;
+    }
+
+    public function testCannotCancelPastAppointment(): void
+    {
+        $appointment = MedicalAppointmentFactory::new()
+            ->scheduled('E4N6ST')
+            ->yesterdayAt('10:00')
+            ->forPatient('John', 'Smith')
+            ->create();
+
+        self::ensureKernelShutdown();
+
+        $this->browser()
+            ->withProfiling()
+            ->post(
+                url: \sprintf('/api/appointments/%s/cancellation', $appointment->getId()),
+                options: HttpOptions::json([
+                    'referenceNumber' => 'E4N6ST',
+                    'lastName' => 'SMITH',
+                    'reason' => 'I booked another one earlier.',
+                ]),
+            )
+            ->assertStatus(422)
+            ->assertJson()
+            ->assertJsonMatches('violations[0].title', 'Appointment is no longer cancellable.')
+            ->use(function (MailerComponent $component): void {
+                $component->assertNoEmailSent();
+            })
+        ;
+    }
+
     protected function browser(): KernelBrowser
     {
         return $this->baseKernelBrowser()
